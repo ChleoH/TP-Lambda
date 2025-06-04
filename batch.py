1160d005c2c8869a4a63d46d9a95740a5bda2fe8
EleaH <chleoeleahinn12345@gmail.com>
2025-06-04 23:31:17 +0200

    Update batch metrics

--- batch.py
+++ batch.py
@@ -1,4 +1,4 @@
-from delta import configure_spark_with_delta_pip 
+from delta import configure_spark_with_delta_pip  
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 from pyspark.sql.types import StructType, StructField, StringType
@@ -15,14 +15,22 @@ schema = StructType([
     StructField("user_agent", StringType(), True)
 ])
 
-df = spark.read.schema(schema).json("data\streaming_logs")
+df = spark.read.schema(schema).json("data/streaming_logs")
 df = df.withColumn("timestamp", to_timestamp(col("timestamp"), "yyyy-MM-dd HH:mm:ss")) \
        .withColumn("date", to_date("timestamp"))
 
-metrics = df.groupBy("ip", "user_agent", "date") \
-            .agg(count("*").alias("connection_count"))
+metrics_by_ip = df.groupBy("ip").agg(count("*").alias("connection_count"))
+metrics_by_ip.write.format("delta") \
+    .mode("overwrite") \
+    .save("data/delta/metrics_by_ip")
 
-metrics.write.format("delta") \
+metrics_by_agent = df.groupBy("user_agent").agg(count("*").alias("connection_count"))
+metrics_by_agent.write.format("delta") \
+    .mode("overwrite") \
+    .save("data/delta/metrics_by_agent")
+
+metrics_by_day = df.groupBy("date").agg(count("*").alias("connection_count"))
+metrics_by_day.write.format("delta") \
     .mode("overwrite") \
     .partitionBy("date") \
-    .save("data/delta/batch_metrics")
+    .save("data/delta/metrics_by_day")
--- streaming2.py
+++ streaming2.py
@@ -1,11 +1,17 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
-import pyspark
+from pyspark.sql.types import StructType, StructField, StringType, TimestampType
 from streaming import logs
 
 spark = SparkSession.builder \
     .appName("StreamLogsToFS") \
     .getOrCreate()
+
+schema = StructType([
+    StructField("timestamp", StringType(), True),
+    StructField("ip", StringType(), True),
+    StructField("user_agent", StringType(), True)
+])
 
 df = spark.readStream \
     .format("kafka") \
@@ -18,13 +24,34 @@ metrics = logs \
     .groupBy("ip", "user_agent", "date") \
     .agg(count("*").alias("connection_count"))
 
-query_metrics = metrics.writeStream \
+metrics_by_ip = df.groupBy("ip").agg(count("*").alias("connection_count"))
+query_ip = metrics_by_ip.writeStream \
+    .outputMode("complete") \
     .format("json") \
-    .option("path", "data/speed_layer/") \
-    .option("checkpointLocation", "data/checkpoint_metrics/") \
+    .option("path", "data/speed_layer/metrics_by_ip") \
+    .option("checkpointLocation", "data/checkpoint/metrics_by_ip") \
     .trigger(processingTime="1 minute") \
-    .partitionBy("date")\
-    .trigger(processingTime="1minute")\
     .start()
 
-query_metrics.awaitTermination()
+metrics_by_agent = df.groupBy("user_agent").agg(count("*").alias("connection_count"))
+query_agent = metrics_by_agent.writeStream \
+    .outputMode("complete") \
+    .format("json") \
+    .option("path", "data/speed_layer/metrics_by_agent") \
+    .option("checkpointLocation", "data/checkpoint/metrics_by_agent") \
+    .trigger(processingTime="1 minute") \
+    .start()
+
+metrics_by_day = df.groupBy("date").agg(count("*").alias("connection_count"))
+query_day = metrics_by_day.writeStream \
+    .outputMode("complete") \
+    .format("json") \
+    .option("path", "data/speed_layer/metrics_by_day") \
+    .option("checkpointLocation", "data/checkpoint/metrics_by_day") \
+    .trigger(processingTime="1 minute") \
+    .start()
+
+query_ip.awaitTermination()
+query_agent.awaitTermination()
+query_day.awaitTermination()
+
